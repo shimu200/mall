d7bbad322a3ab733507ebc256cbf8654cf1dc437
shimu <3039375697@qq.com>
2023-04-05 21:22:05 +0800

    4.5晚

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,18 +1,44 @@
 package com.atguigu.gulimall.product;
 
+
+
+
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+//import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
+
+    @Test
+    public void testUpload()throws FileNotFoundException {
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        String accessKeyId = "LTAI5tQhWGP5NtcMLSAjghUN";
+        String accessKeySecret = "a20VqufSscT5E0dKI69hMs2FmltxC6";
+        OSS ossClient = new OSSClientBuilder().build(endpoint,accessKeyId,accessKeySecret);
+        InputStream inputStream = new FileInputStream("E:\\硅谷课堂\\课件和文档\\基础篇\\资料\\pics\\apple.png");
+        ossClient.putObject("gulimall-hello303","apple.png",inputStream);
+        ossClient.shutdown();
+        System.out.println("上传成功");
+    }
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
