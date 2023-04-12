005194b29cfe07674a2a88dcf6216b52a78dab0f
shimu <3039375697@qq.com>
2023-04-12 21:26:35 +0800

    95

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
@@ -34,6 +34,7 @@ public class WareInfoController {
      * 列表
      */
     @RequestMapping("/list")
+    //@RequiresPermissions("ware:wareinfo:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = wareInfoService.queryPage(params);
 
