1ee4d34c46a5650c91edfcd196c9c9f2e8736318
shimu <3039375697@qq.com>
2023-05-10 17:43:47 +0800

    243

--- gulimall-cart/src/main/java/com/atguigu/gulimall/cart/controller/CartController.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/controller/CartController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.concurrent.ExecutionException;
 
@@ -26,9 +27,20 @@ public class CartController {
     @GetMapping("/addToCart")
     public String addToCart(@RequestParam("skuId") Long skuId,
                             @RequestParam("num") Integer num,
-                            Model model) throws ExecutionException, InterruptedException {
-        CartItem cartItem = cartService.addToCart(skuId,num);
-        model.addAttribute("item",cartItem);
+                            RedirectAttributes ra) throws ExecutionException, InterruptedException {
+//        CartItem cartItem = cartService.addToCart(skuId,num);
+//        model.addAttribute("item",cartItem);
+        cartService.addToCart(skuId,num);
+        ra.addAttribute("skuId",skuId);
+        return "redirect:http://cart.gulimall.com/addToCartSuccess.html";
+    }
+
+    @GetMapping(value = "/addToCartSuccess.html")
+    public String addToCartSuccessPage(@RequestParam("skuId") Long skuId,
+                                       Model model) {
+        //重定向到成功页面。再次查询购物车数据即可
+        CartItem cartItem = cartService.getCartItem(skuId);
+        model.addAttribute("item", cartItem);
         return "success";
     }
 }
